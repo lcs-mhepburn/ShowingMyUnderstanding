fcb86df319bff84a436b74dee59e7ef44794a3ac
Testing <testing@lcs-mba2410.lcs.on.ca>
2017-11-23 11:18:41 -0500

    Got the top of the balloons done, need to do sticks

--- ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
@@ -30,6 +30,8 @@ import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
+canvas.drawRectangle(centreX: 150, centreY: 150, width: 300, height: 300)
+
 
 // No borders
 canvas.drawShapesWithBorders = false
@@ -38,6 +40,36 @@ canvas.drawShapesWithBorders = false
 canvas.defaultLineWidth = 5
 
 // Add your code below... remember to use comments to indicate your intent
+//Green balloon
+canvas.fillColor = Color(hue: 120, saturation: 80, brightness: 90, alpha: 100)
+canvas.drawEllipse(centreX: 75, centreY: 200, width: 100, height: 100)
+
+//Yellow Balloon
+canvas.fillColor = Color(hue: 60, saturation: 80, brightness: 90, alpha: 100)
+canvas.drawEllipse(centreX: 125, centreY: 200, width: 100, height: 100)
+
+//Blue Balloon
+canvas.fillColor = Color.init(hue: 240, saturation: 80 , brightness: 90, alpha: 100)
+canvas.drawEllipse(centreX: 225, centreY: 200, width: 100, height: 100)
+
+//Red Balloon
+canvas.fillColor = Color.init(hue: 0, saturation: 70, brightness: 100, alpha: 80)
+canvas.drawEllipse(centreX: 175, centreY: 200, width: 100, height: 100)
+
+
+//green balloon
+canvas.fillColor = Color.init(hue: 120, saturation: 80, brightness: 90, alpha: 50)
+canvas.drawEllipse(centreX: 75, centreY: 200, width: 100, height: 100)
+
+//Yellow Balloon
+canvas.fillColor = Color(hue: 60, saturation: 80, brightness: 90, alpha: 50)
+canvas.drawEllipse(centreX: 125, centreY: 200, width: 100, height: 100)
+
+//blue balloon
+canvas.fillColor = Color(hue: 240, saturation: 80, brightness: 90, alpha: 50)
+canvas.drawEllipse(centreX: 225, centreY: 200, width: 100, height: 100)
+
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
