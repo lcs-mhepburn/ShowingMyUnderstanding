1559e71e8d9fbc25e3dab6e8907285810dab072d
Hepburn, Magnus <mhepburn@lcs-mba3116.lcs.on.ca>
2017-11-25 11:41:49 -0500

    complete conditionals example

--- ShowingMyUnderstanding.playground/Pages/Conditionals - Practice.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Conditionals - Practice.xcplaygroundpage/Contents.swift
@@ -29,7 +29,11 @@ let canvas = Canvas(width: 300, height: 300)
 let number = random(from: 0, toButNotIncluding: 2)
 
 // Replace this comment and add your code below...
-
+if number == 0 {
+    canvas.drawRectangle(centreX: 150, centreY: 150, width: 200, height: 200)
+} else {
+    canvas.drawEllipse(centreX: 0, centreY: 0, width: 220, height: 220 )
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
