7a12a2a974d9c5d5f2f31a7c8c767b3153df8549
Hepburn, Magnus <mhepburn@lcs-mba3116.lcs.on.ca>
2017-11-25 11:56:01 -0500

    i made an example and completed

--- ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -20,7 +20,29 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 // Replace this command and add your code below...
+// Generate a random number
+let number = random(from: 0, toButNotIncluding: 4)
 
+// Replace this comment and add your code below...
+
+//black
+if number == 0 {
+//blue
+} else if number == 1 {
+    canvas.fillColor = Color.init(hue: 240, saturation: 80 , brightness: 90, alpha: 100)
+    // yellow
+} else if number == 2 {
+    canvas.fillColor = Color(hue: 60, saturation: 80, brightness: 90, alpha: 100)
+//green
+} else if number == 3 {
+    
+    canvas.fillColor = Color.init(hue: 120, saturation: 80, brightness: 90, alpha: 50)
+}
+  
+
+// Draw a circle
+    
+    canvas.drawEllipse(centreX: 150, centreY: 150, width: 200, height: 200)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
