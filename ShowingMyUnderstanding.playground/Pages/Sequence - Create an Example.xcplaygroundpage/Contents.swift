c17ee6406ee6587f4fb692758ceaed8c88d38543
Hepburn, Magnus <mhepburn@lcs-mba3116.lcs.on.ca>
2017-11-25 11:25:54 -0500

    draw face

--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -25,8 +25,25 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Replace this comment and add your code below...
 
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
- */
+
+//face
+canvas.fillColor = Color.init(hue: 240, saturation: 80 , brightness: 90, alpha: 100)
+
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 200, height: 200)
+
+// left eye
+
+canvas.fillColor = Color.init(hue: 0, saturation: 70, brightness: 100, alpha: 100)
+
+canvas.drawEllipse(centreX: 180, centreY: 180, width:20, height: 20)
+
+//right eye
+canvas.fillColor = Color.init(hue: 0, saturation: 70, brightness: 100, alpha: 100)
+canvas.drawEllipse(centreX: 120, centreY: 180, width:20, height: 20)
+
+//mouth
+canvas.fillColor = Color.init(hue: 0, saturation: 70, brightness: 100, alpha: 100)
+
+canvas.drawRectangle(centreX: 150, centreY: 120, width: 100, height: 20)
+
 PlaygroundPage.current.liveView = canvas.imageView
